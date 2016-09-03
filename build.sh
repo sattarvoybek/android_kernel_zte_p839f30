4fea573c98f9408e5939c40c1d4bcee21f15903e
Sattarvoybek <sattarvoybek@github.com>
2016-09-04 00:28:07 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE="/home/sattarvoybek/android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"
+
+
+make msm8939-blade_defconfig
+make -j16
+
+./dtbToolCM -2 -o dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/dts/
+
+#cp ./arch/arm/boot/zImage ./out/zImage
+
