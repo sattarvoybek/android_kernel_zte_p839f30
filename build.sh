25a261f001ff21514df49278f955239a1df1cdf8
Sattarvoybek <sattarvoybek@github.com>
2016-09-14 17:49:42 +0300

    del

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE="/home/sattarvoybek/android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"
+export CROSS_COMPILE="/home/sattarvoybek/android_prebuilt_toolchains/arm-eabi-5.3/bin/arm-eabi-"
 
 
 make msm8939-blade_defconfig
