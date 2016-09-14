879cc93ae98cd750b445a56363724b9e29060bed
Sattarvoybek <sattarvoybek@github.com>
2016-09-14 22:01:16 +0300

    add Metro_defconfig

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export ARCH=arm
+export KBUILD_BUILD_USER="Sattarvoybek"
+export KBUILD_BUILD_HOST="Sattar-Server"
 export CROSS_COMPILE="/home/sattarvoybek/android_prebuilt_toolchains/arm-eabi-5.3/bin/arm-eabi-"
 
 
-make msm8939-blade_defconfig
+make Metro_defconfig
 make -j16
 
 ./dtbToolCM -2 -o dt.img -s 2048 -p scripts/dtc/ arch/arm/boot/dts/
